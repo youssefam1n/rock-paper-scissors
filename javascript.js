c1a2bc9345206bc9f40b709582679a5478d29c85
youssefam1n <y.01002231098@gmail.com>
2025-07-20 05:18:44 +0300

    get computer's choice done

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+  let randInt = getRandomInt(3);
+  switch (randInt) {
+    case 0:
+      return "Rock";
+      break;
+    case 1:
+      return "Paper";
+    case 2:
+    return "Scissors";
+  }
+}
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
